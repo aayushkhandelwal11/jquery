926535c68f6456cb867840f2b91bc18a0b946505
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-07-14 17:47:10 +0530

    jsoncontent

--- exercises/exercises/loadcontentusingjson/js/specials.js
+++ exercises/exercises/loadcontentusingjson/js/specials.js
@@ -5,10 +5,27 @@ $(function(){
     $(form).
     children('ul').
     change(function(){
-      $.getJSON('data/specials.json', function(my_json) {
-      /* $.each(resp, function(k, v) {
-          console.log(v[key]);
-       });*/
+    
+       $.ajax({
+       url: 'data/specials.json',
+         cache: true,
+        dataType: "json",
+      success: function(my_json) {
+           for (var key in my_json) {
+                 if($("select[name=day]").val()==key)
+                    {  $("#division").children().remove();
+            						 $("#division").css("background-color",my_json[key].color); 
+           						 $("#division").append("<h2>"+my_json[key].title+"</h2><br><img src="+my_json[key].image+"><br><h6>"+my_json[key].text+"</h6></img>")
+         
+                       $(".buttons").remove();
+           
+                    }
+           }
+       }
+   
+     });
+   /*   $.getJSON('data/specials.json', function(my_json) {
+    
         for (var key in my_json) {
         if($("select[name=day]").val()==key)
         {  $("#division").children().remove();
@@ -21,7 +38,7 @@ $(function(){
        }
    
     
-    });
+    });*/
 
    });
 
