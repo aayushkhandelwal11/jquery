5b50e1fbd388cb1a18b0b8ab78d7a85ad6f3ab35
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-07-14 17:37:16 +0530

    jsoncontent

--- exercises/exercises/loadcontentusingjson/js/specials.js
+++ exercises/exercises/loadcontentusingjson/js/specials.js
@@ -15,7 +15,7 @@ $(function(){
             $("#division").css("background-color",my_json[key].color); 
            $("#division").append("<h2>"+my_json[key].title+"</h2><br><img src="+my_json[key].image+"><br><h6>"+my_json[key].text+"</h6></img>")
          
-           console.log($("#division").children(""))
+           $(".buttons").remove();
            
         }
        }
