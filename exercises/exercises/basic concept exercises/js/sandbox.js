4e15de456021eefb2c771717ae3e0f0506238645
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-07-17 11:04:26 +0530

    last commit

--- exercises/exercises/basic concept exercises/js/sandbox.js
+++ exercises/exercises/basic concept exercises/js/sandbox.js
@@ -4,10 +4,10 @@ $(function(){
     console.log($("#myList :eq(2)"))//i found this more suitable as directly the child is found wheres in other all childs are searched
     console.log($("#myList :nth-child(3)"))
     console.log($($("#myList li")[2]))
-    console.log($("#search").find("label"))
+    console.log( $("label[for=q]") )
     console.log($(":hidden").length)
     console.log($("[alt]").length)
-    console.log($("table").find("tr:even"))
+    console.log($("table").find("tr:odd"))
     console.log("traversing");
     
     $("img").each(function(idx, el) {
@@ -15,22 +15,13 @@ $(function(){
      });
    
    
-    $("input", document.body).each(function () {
+    $("input[type=text]", document.body).each(function () {
+       console.log($(this))
        var parentTag = $(this).parent("form")
        parentTag.attr("class","input_text")
      });
     
-   
-     $("#myList").
-      children('li.current').
-        each(function(idx,el){
-           if($(el).attr("class")=="current")
-           {
-              $(el).removeClass('current') ;
-              $(el).next().addClass('current')
-           }
-     });
-  
+  $("#myList li.current").removeClass('current').next().addClass('current') ;
      
      
      $("#specials").
@@ -42,21 +33,21 @@ $(function(){
               }
          });
      
-   var firstelement=$("#slideshow :eq(0)") ;
-   $(firstelement).addClass('current'); 
-   $(firstelement).nextAll().addClass('disabled');    
+     var firstelement=$("#slideshow :first") ;
+     $(firstelement).addClass('current'); 
+     $(firstelement).nextAll().addClass('disabled');    
    
    for(i=1;i<6;i++)
    {
      $('#myList').append('<li> text no' + i + '</li>');
    }   
    
-   $('#myList :odd').remove();
+   /*$('#myList :odd').remove();*/
    
    $('div.module:last').append('<h2>   new heading inserted</h2>')
    $('div.module:last').append('<p>   new paragraph isnserted after the heading</p>')
    $("div.module select").append('<option value="wednesday">Wednesday</option>');
-   $("body").append('<div class="module"></div>')
+   $("div.module :last").append('<div class="module"></div>')
    $('img:last').clone().appendTo($('div.module:last'))
    
     
