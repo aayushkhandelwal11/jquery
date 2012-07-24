a1b5a31a148320f37916503dd156bc5cf28acc64
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-07-24 11:17:36 +0530

    accordian

--- exercises/exercises/accordian_implementation/load.js
+++ exercises/exercises/accordian_implementation/load.js
@@ -1,22 +1,6 @@
 $(function(){
    var parms = new Array();
    var query = window.location.search.substring(1);
-   var parents = $("#"+query).children().parents("*")
-   for (var i = parents.length-4; i >= 0; i--) {
-           if(parents[i].id.length!=0) {
-                console.log($("#"+parents[i].id))
-                $("#"+parents[i].id).css("display","block");
-               $("#"+parents[i].id).children("a").show();
-               $("#"+parents[i].id).children("a").focus();
-      
-               $.browser.chrome = /chrome/.test(navigator.userAgent.toLowerCase()); 
-               if($.browser.chrome) {
-                     $("#"+parents[i].id).children("a").click();
-               }
-            }
-         
-           $("#"+query).children("a").css("background-color","green")
-          
-   
-   }
+   $("#"+query).parents().show().focus()
+   $("#"+query).children("a").css("background-color","green")
 });
