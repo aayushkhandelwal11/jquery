6dd7499155c075ba41fd83ab544ffe61d6fa72d2
aayushkhandelwal11 <aayush.khandelwal@vinsol.com>
2012-08-08 16:48:25 +0530

    final commit

--- exercises/exercises/handson/ej.js
+++ exercises/exercises/handson/ej.js
@@ -7,19 +7,20 @@ var counter=0
   counter++
  });
 
-    x=$("tr").length
+
     var found=0
-   	for(i=1;i<Number(x);i++) {
+   	$("tr td").parent().each(function(){
    	 	  ct=0;
-	   	  var u =$("tr")[i]
+	   	 
 		   	while (found!=1&&ct<counter) {  
-		     if($(u).children()[ct].innerText>j[ct]) {
-		 	       $(u).before("<tr><td>"+j[0]+"</td><td>"+j[1]+"</td><td>"+j[2]+"</td></tr>")
+		     if($(this).children()[ct].innerText>j[ct]) {
+		 	       $(this).before("<tr><td>"+j[0]+"</td><td>"+j[1]+"</td><td>"+j[2]+"</td></tr>")
 		 	       found=1 
 		 	   }
- 		 	   ct= $(u).children()[ct].innerText<j[ct] ? counter:ct+1
+ 		 	   ct= $(this).children()[ct].innerText<j[ct] ? counter:ct+1
 		 	 }
-		 }
+   
+   });
    if(found==0) {
      $("table").append("<tr><td>"+j[0]+"</td><td>"+j[1]+"</td><td>"+j[2]+"</td></tr>")
    }
